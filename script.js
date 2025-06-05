f5cb94849a21cf558c4fba865f9e069c74ef8fdf
PSR Indústria <marketingpsrimpressao@gmail.com>
2025-06-05 19:41:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -672,7 +672,7 @@ yCol2 += finalidadeBoxHeight + 5;
 
   // --- Assinaturas ---
   currentY += 15;
-  const signatureY = Math.min(currentY, doc.internal.pageSize.getHeight() - 20);
+  const signatureY = Math.min(currentY, doc.internal.pageSize.getHeight() - 15);
   const signatureLineLength = 60;
   const signatureCol1X = margin + contentWidth * 0.15;
   const signatureCol2X = pageWidth - margin - contentWidth * 0.15 - signatureLineLength;
